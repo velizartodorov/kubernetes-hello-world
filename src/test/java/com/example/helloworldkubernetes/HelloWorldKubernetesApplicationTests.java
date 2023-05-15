d9451eee516dad9db107023d1b91ff1c18666585
velizartodorov <willking66@gmail.com>
2023-05-15 10:39:39 +0200

    :sparkles: push "Hello World" code

---
+++ src/test/java/com/example/helloworldkubernetes/HelloWorldKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldkubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
